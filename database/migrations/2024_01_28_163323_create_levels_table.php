ba763d8d0816779ebfc34f03504efd5dc1b19e60
Rinardo Paparella <rinardo.paparella1996@gmail.com>
2024-01-28 19:53:46 -0400

    fix: bug migration

--- database/migrations/2024_01_28_163323_create_levels_table.php
+++ database/migrations/2024_01_28_163323_create_levels_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('levels', function (Blueprint $table) {
             $table->id();
             $table->string('name', 150);
-
             $table->timestamps();
         });
     }
